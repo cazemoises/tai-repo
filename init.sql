3ec2ca49434e3be2ad5ba0500327f17f94417d12
Moises Cazé <cazemoises2@gmail.com>
2024-05-03 00:21:30 -0300

    .

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  "password" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  "description" TEXT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
